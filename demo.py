6221110e7c2eac45197712f56ed89de30ca79519
Luojiaji <lt920@126.com>
2019-06-27 16:51:58 +0800

    add code of load dataset

--- demo.py
+++ demo.py
@@ -23,3 +23,11 @@ x = Dense(10, activation='softmax', name='fc2')(x)
 
 model = Model(input_data, x)
 model.summary()
+
+
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
